fec084c0df88573ade290e63a778601d298c92fe
byeolee1221 <zicsso1221@gmail.com>
2024-10-16 23:02:48 +0900

    feat: 테일윈드 config파일 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 export default function Home() {
   return (
-    <div>test in develop</div>
+    <>
+      <div className="text-xs leading-5">test in develop</div>
+      <div className="text-sm">test in develop</div>
+      <div className="text-base">test in develop</div>
+      <div className="text-lg">test in develop</div>
+      <div className="text-xl">test in develop</div>
+      <div className="text-2xl">test in develop</div>
+      <div className="text-3xl">test in develop</div>
+      <div className="text-4xl">test in develop</div>
+      <div className="text-4xl text-red01">test in develop</div>
+      
+
+    </>
   );
 }
